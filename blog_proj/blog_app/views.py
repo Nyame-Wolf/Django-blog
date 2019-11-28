ab0461468b68d0b985f97f32ccc993d40aa724cc
Nyame-Wolf <mumenyam@gmail>
2019-11-28 12:52:21 +0300

    Create view funcs for home and about pages

--- blog_proj/blog_app/views.py
+++ blog_proj/blog_app/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    context = {
+        'posts': Post.objects.all()
+        }
+    return render(request, 'blog_app/home.html', context)
+
+def about(request):
+    return render(request, 'blog_app/about.html'{'title':'About'})
