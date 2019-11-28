241a3695004d35ca48872996a99446e7424f4735
Nyame-Wolf <mumenyam@gmail>
2019-11-28 14:40:50 +0300

    Add a model to handle Posts

--- blog_proj/blog_app/models.py
+++ blog_proj/blog_app/models.py
@@ -1,5 +1,15 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
 
 class CustomUser(AbstractUser):
     pass
+
+class Post(models.Model):
+     title = models.CharField(max_length=100)
+     content = models.TextField()
+     date_posted = models.DateTimeField(default=timezone.now)
+     author = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
+     
+     def __str__(self):
+          return self.title
