a21f77ec42fcf87b8835ca8fd89f3ba3f29c61de
Nyame-Wolf <mumenyam@gmail>
2019-11-29 17:32:41 +0300

    Add a logout CBV url

--- blog_proj/blog_proj/urls.py
+++ blog_proj/blog_proj/urls.py
@@ -23,4 +23,6 @@ urlpatterns = [
     path('', include('blog_app.urls')),
     path('register/', user_views.register, name='register'),
     path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
+#template_name='users/login.html'
