64cafb0f73a146afce8783c6c672e077582c106a
Nyame-Wolf <mumenyam@gmail>
2019-11-29 10:30:02 +0300

    Populate the forms with information entered

--- blog_proj/users/views.py
+++ blog_proj/users/views.py
@@ -3,7 +3,10 @@ from blog_app.forms import CustomUserCreationForm
 
 
 def register(request):
-    form = CustomUserCreationForm()
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+    else:
+        form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
 # Create your views here.
