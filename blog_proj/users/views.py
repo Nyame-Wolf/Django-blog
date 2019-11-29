557d51f1eb956226b772f042234d17797ce77b41
Nyame-Wolf <mumenyam@gmail>
2019-11-29 10:24:28 +0300

    Add a user register view and map it to project urls

--- blog_proj/users/views.py
+++ blog_proj/users/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from blog_app.forms import CustomUserCreationForm
+
+
+def register(request):
+    form = CustomUserCreationForm()
+    return render(request, 'users/register.html', {'form': form})
 
 # Create your views here.
