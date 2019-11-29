d3b9aa864ab148b34d1a4f37ab05bbe10a6e0481
Nyame-Wolf <mumenyam@gmail>
2019-11-29 10:32:29 +0300

    Validate the data form the form

--- blog_proj/users/views.py
+++ blog_proj/users/views.py
@@ -1,12 +1,13 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render
 from blog_app.forms import CustomUserCreationForm
 
 
 def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('usename')
     else:
         form = CustomUserCreationForm()
     return render(request, 'users/register.html', {'form': form})
 
-# Create your views here.
