699ce9f8010783a5ce53260adf452a80c295edcc
Nyame-Wolf <mumenyam@gmail>
2019-12-03 01:22:45 +0300

    add a CBV DetailView, map it to its url and create its tempate

--- blog_proj/blog_app/views.py
+++ blog_proj/blog_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import (ListView,
+DetailView
+)
 
 # def home(request):
 #     context = {
@@ -13,6 +15,9 @@ class PostListView(ListView):
     context_object_name = 'posts'
     template_name='blog_app/home.html'
     ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+    model = Post
 
 def about(request):
     return render(request, 'blog_app/about.html',{'title':'About'})
