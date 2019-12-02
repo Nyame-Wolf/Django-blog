08348e012339c119e3b8cda02bbb99e1acde1de7
Nyame-Wolf <mumenyam@gmail>
2019-12-02 09:56:09 +0300

    Set path where image is saved and ensure browser finds it irrespective of os

--- blog_proj/blog_proj/settings.py
+++ blog_proj/blog_proj/settings.py
@@ -122,6 +122,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
+MEDIA_ROOT = os.path.join(BASE_DIR,'media')
+
+MEDIA_URL = '/media/'
+
 AUTH_USER_MODEL = 'blog_app.CustomUser'
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
