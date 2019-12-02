6f8c7e079f6b6206cfc71ac9434376699219ca28
Nyame-Wolf <mumenyam@gmail>
2019-12-03 01:08:04 +0300

    add a CBV ListView

--- blog_proj/blog_app/views.py
+++ blog_proj/blog_app/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView
 
-def home(request):
-    context = {
-        'posts': Post.objects.all()
-        }
-    return render(request, 'blog_app/home.html', context)
+# def home(request):
+#     context = {
+#         'posts': Post.objects.all()
+#         }
+#     return render(request, 'blog_app/home.html', context)
+
+class PostListView(ListView):
+    model = Post
+    context_object_name = 'posts'
+    template_name='blog_app/home.html'
+    ordering = ['-date_posted']
 
 def about(request):
     return render(request, 'blog_app/about.html',{'title':'About'})
