8f7236e3112f00626cd59b9e9283d85d2f8c7ec3
Nyame-Wolf <mumenyam@gmail>
2019-12-02 11:34:40 +0300

    Refactor views to update users account

--- blog_proj/users/views.py
+++ blog_proj/users/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render,redirect
 from blog_app.forms import CustomUserCreationForm
 from django.contrib import messages 
 from django.contrib.auth.decorators  import login_required
-
+from .forms import UserUpdateForm, ProfileUpdateFrom
 
 def register(request):
     if request.method == 'POST':
@@ -18,4 +18,18 @@ def register(request):
 
 @login_required
 def profile(request):
-    return render(request, 'users/profile.html',)
+    if request.method == 'POST':
+        u_form = UserUpdateForm(request.POST, instance=request.user)
+        p_form = ProfileUpdateFrom(request.POST, request.FILES, instance=request.user.profile )
+        if u_form.is_valid() & p_form.is_valid():
+            p_form.save()
+            u_form.save()
+            messages.success(request, f'Your Account has been updated!')
+    else:
+         u_form = UserUpdateForm(instance=request.user)
+         p_form = ProfileUpdateFrom(instance=request.user.profile )
+    context = {
+        'u_form' : u_form,
+        'p_form' : p_form
+    }
+    return render(request, 'users/profile.html',context)
