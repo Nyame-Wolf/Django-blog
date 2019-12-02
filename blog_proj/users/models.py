9ccc888407b321f8e7713cdbcd23a8e56b48ccdd
Nyame-Wolf <mumenyam@gmail>
2019-12-02 11:03:03 +0300

    Automatically resize dpics

--- blog_proj/users/models.py
+++ blog_proj/users/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from PIL import Image
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
@@ -7,4 +8,15 @@ class Profile(models.Model):
 
     def __str__(self):
         return f'{self.user.username} Profile'
+    
+
+    def save(self, *args, **kwargs):
+       super().save(*args, **kwargs) # Call the real save() method
+
+       img = Image.open(self.image.path)
+
+       if img.height > 300 or img.width > 300:
+           output_size = (300,300)
+           img.thumbnail(output_size)
+           img.save(self.image.path)
 
