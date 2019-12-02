61c25931bed1cf2b6a525f23906af855a43e9001
Nyame-Wolf <mumenyam@gmail>
2019-12-03 02:28:17 +0300

    A post update CBV and map it to url

--- blog_proj/blog_app/views.py
+++ blog_proj/blog_app/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from .models import Post
 from django.views.generic import (ListView,
 DetailView,
-CreateView
+CreateView,
+UpdateView
 )
 from django.contrib.auth.mixins import LoginRequiredMixin
 
@@ -22,6 +23,15 @@ class PostDetailView(DetailView):
     model = Post
 
 class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    fields = ['title', 'content']
+
+    def form_valid(self,form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostUpdateView(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'content']
 
